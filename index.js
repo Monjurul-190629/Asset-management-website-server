7db1dc9017612bac76e8709088b8dd14c1b2dafe
Monjurul Alam <monjurulalam190629@gmail.com>
2024-06-22 03:39:21 +0600

    on delete is fine

--- index.js
+++ index.js
@@ -226,6 +226,25 @@ async function run() {
             res.send(result);
         })
 
+        
+        app.get('/assets/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            console.log(query)
+            const result = await assetCollection.findOne(query)
+            res.send(result)
+        });
+
+        // For delete
+        app.delete('/assets/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await assetCollection.deleteOne(query)
+            res.send(result)
+        })
+        
+        
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
