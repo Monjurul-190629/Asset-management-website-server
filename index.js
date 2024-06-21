493039c7cae37e73958fad7f13dd5525370abe90
Monjurul Alam <monjurulalam190629@gmail.com>
2024-06-21 23:50:52 +0600

    some eevnet is ok

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.Stripe_secret_key);
 const port = process.env.PORT || 5000;
 
 
@@ -12,7 +14,7 @@ app.use(express.json());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.5u6pxxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +32,33 @@ async function run() {
         await client.connect();
         /// database section
         const userCollection = client.db("ServiceProviderDB").collection('users');
+        const paymentCollection = client.db("ServiceProviderDB").collection("payments");
+        const companyCollection = client.db("ServiceProviderDB").collection("company");
+
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.Token_secret, { expiresIn: '24h' })
+            res.send({ token })
+        })
+
+        /// verify token
+        /// middleware
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.Token_secret, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
 
 
         /// for the user
@@ -52,6 +81,135 @@ async function run() {
             res.send(result);
         })
 
+        /// manager
+        app.get('/users/manager/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let manager = false;
+            if (user) {
+                manager = user?.role === 'HR_manager';
+            }
+            res.send({ manager });
+        })
+        /// manager get logo and other details
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            res.send(user);
+            console.log(user)
+        })
+
+
+        /// payment
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            console.log(amount, 'amount ')
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        });
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const paymentResult = await paymentCollection.insertOne(payment);
+            res.send(paymentResult);
+        })
+
+        app.get('/payments', async (req, res) => {
+            const result = await paymentCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/payments/:email', async (req, res) => {
+            const query = { email: req.params.email }
+            const result = await paymentCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        //// company collection
+        app.post('/companyHolder', async (req, res) => {
+            const user = req.body;
+            // insert email if it doesn't exists
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await companyCollection.insertOne(user);
+            res.send(result);
+        })
+
+        /// get
+        app.get('/companyHolder', async (req, res) => {
+            console.log(req.headers)
+            const result = await companyCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/companyHolder/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await companyCollection.findOne(query);
+            res.send(user);
+            console.log(user)
+        })
+
+
+        app.put('/companyHolder/:email', async (req, res) => {
+            const email = req.params.email;
+            const { limit, Employee_count } = req.body;
+
+            try {
+                // Find the document with the specified email and update the limit
+                const updatedCompanyHolder = await companyCollection.findOneAndUpdate(
+                    { email: email },
+                    { $set: { limit: limit, Employee_count: Employee_count } },
+                    { new: true } // Return the updated document
+                );
+
+                if (!updatedCompanyHolder) {
+                    return res.status(404).json({ error: 'Company holder not found' });
+                }
+
+                res.json(updatedCompanyHolder); // Send back the updated company holder document
+            } catch (error) {
+                console.error('Error updating limit:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        /// update empolyee
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const user = req.body;
+
+            const updateDoc = {
+                $set: {
+                    name: user.name,
+                    email: user.email,
+                    Date_of_Birth: user.Date_of_Birth,
+                    role: user.role,
+                    Profile_image: user.Profile_image,
+                    Company_name: user.Company_name,
+                    Company_logo: user.Company_logo
+                },
+            };
+            console.log(updateDoc)
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
